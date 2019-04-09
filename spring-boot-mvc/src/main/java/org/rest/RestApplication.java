85a662cc1fe01a70c9ffd8ab906a90d3de7bed05
Restu Maulunida <vandalz12@gmail.com>
2019-04-09 23:25:25 +0700

    add swagger for rest api documentation

--- spring-boot-mvc/src/main/java/org/rest/RestApplication.java
+++ spring-boot-mvc/src/main/java/org/rest/RestApplication.java
@@ -2,20 +2,12 @@ package org.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class RestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApplication.class, args);
-	}
-	
-	@RequestMapping("/greet")
-	public String helloGreeting() {
-		return "Hello REST";
 	}
 	
 }
